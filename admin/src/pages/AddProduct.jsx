4804666bb8d8186e80a62f1c7d9ac7a38dfd309b
Rut Birhanu <birhanurut@gmail.com>
2024-10-27 22:48:26 +0300

    upload product image

--- admin/src/pages/AddProduct.jsx
+++ admin/src/pages/AddProduct.jsx
@@ -8,15 +8,36 @@ function AddProduct() {
     name: "",
     description: "",
     price: "",
-    category:""
+    category: "",
+    image:""
   })
   
-  const handleOnChange = (e) => {
-   setFormData({...formData, [e.target.name]:e.target.value})
-  }
 
-  const handleSubmit = {
-    
+  const handleOnChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData((prevData) => ({
+        ...prevData,
+        [name]: files ? files[0] : value 
+    }));
+};
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formDataToSend = new FormData();
+    formDataToSend.append("name", formData.name);
+    formDataToSend.append("description", formData.description);
+    formDataToSend.append("category", formData.category);
+    formDataToSend.append("price", formData.price);
+    formDataToSend.append("image", formData.image);
+
+    const req = await fetch("http://localhost:3500/product/add-product", {
+      method: "POST",
+      body:formDataToSend
+    })
+    const response= await req.json()
+    console.log(response)
   }
 
   return (
@@ -26,7 +47,6 @@ function AddProduct() {
         <div className="main-content">
           <div className="main-title-container">
             <h2>Add Product</h2>
-            <ButtonComponent />
           </div>
 
           <div className="add-product-form">
@@ -53,16 +73,21 @@ function AddProduct() {
                   <label htmlFor="price">Price</label>
                   <input type="text" name="price" onChange={handleOnChange} value={formData.price}/>
                 </div>
-              </form>
-            </div>
+            <ButtonComponent />
+
+           
 
             <div className="right-side">
               <h2>Upload Image</h2>
+              <input type="file" name="image" onChange={handleOnChange} />
               <div className="product-image">
                 <img src="https://assets.adidas.com/images/w_600,f_auto,q_auto/f2d9229b65c248488c78af3b00851dab_9366/Runfalcon_3.0_Shoes_White_HP7557_01_standard.jpg" />
                 <button>Browse</button>
               </div>
+
             </div>
+              </form>
+              </div>
           </div>
         </div>
       </div>
