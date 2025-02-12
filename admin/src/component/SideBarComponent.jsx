a9e5acd3997f663b0c9fab787904cb1df49cb0c0
Rut Birhanu <birhanurut@gmail.com>
2025-02-12 18:19:37 +0300

    sidebar structure update

--- admin/src/component/SideBarComponent.jsx
+++ admin/src/component/SideBarComponent.jsx
@@ -1,19 +1,20 @@
 import "./component.css"
 import { Link } from 'react-router-dom';
-
+import { FaUsers, FaBoxOpen, FaShoppingCart, FaUserCircle } from 'react-icons/fa';
 
 function SideBarComponent() {
   return (
-      <div className="side-bar-container">
-          <h2>Dashboard</h2>
-          <div>
-              <Link to="/customers"><p>Users</p></Link>
-             <Link to="/products"><p>Products</p></Link> 
-              <Link to ="/orders"><p>Orders</p></Link>
-              <p>Customers</p>
-          </div>
+    <div className="side-bar-container">
+      <h2 className="logo-title">🧾 MyDashboard</h2>
+      <div className="nav-links">
+        <Link to="/customers" className="nav-link"><FaUsers /> <span>Users</span></Link>
+        <Link to="/products" className="nav-link"><FaBoxOpen /> <span>Products</span></Link>
+        <Link to="/orders" className="nav-link"><FaShoppingCart /> <span>Orders</span></Link>
+        <div className="nav-link inactive-link"><FaUserCircle /> <span>Customers</span></div>
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default SideBarComponent
