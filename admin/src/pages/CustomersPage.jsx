cb8340c37773a66c3d5febf0c1c684426300ba69
Rut Birhanu <birhanurut@gmail.com>
2025-02-14 19:39:51 +0300

    modify dashboard table

--- admin/src/pages/CustomersPage.jsx
+++ admin/src/pages/CustomersPage.jsx
@@ -6,38 +6,40 @@ import { fetchUsers } from "../redux/userSlice"
 
 function CustomersPage() {
   const dispatch = useDispatch()
-  const {users}= useSelector(state=>state.user)
+  const { users } = useSelector(state => state.user)
 
   useEffect(() => {
     dispatch(fetchUsers())
   }, [dispatch])
 
   return (
-    <div className="customer-page-container">
+    <div className="orders-page-container">
       <SideBarComponent />
-      <div className="customers-page-content">
+      <div className="orders-page-content">
         <h2>Customers</h2>
-        <div className="customers-list">
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Email</th>
-              </tr>
-            </thead>
+        <div className="orders-table-wrapper">
+          <div className="table-wrapper">
+            <table className="orders-table">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
 
-            <tbody>
-              {
-                users.map(user => {
-                  return  <tr key={user._id}>
-                    <td>{user._id}</td>
-                    <td>{user.email}</td>
-                </tr>
-                })
-              }
-             
-            </tbody>
-          </table>
+                <tbody>
+                  {
+                    users.map(user => {
+                      return <tr key={user._id}>
+                        <td>{user._id}</td>
+                        <td>{user.email}</td>
+                      </tr>
+                    })
+                  }
+
+                </tbody>
+              </table>
+          </div>
         </div>
       </div>
     </div>
