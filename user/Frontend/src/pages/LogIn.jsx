cf0f02883f264d9fe4702d97cf93394d558d44d4
Rut Birhanu <birhanurut@gmail.com>
2025-09-27 21:41:31 +0300

    refactor: clean up authentication pages

--- user/Frontend/src/pages/LogIn.jsx
+++ user/Frontend/src/pages/LogIn.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "./auth.css"
 import { useEffect, useState } from 'react';
 import { useDispatch } from "react-redux";
-import { login} from '../redux/authSlice';
+import { login } from '../redux/authSlice';
 import { useNavigate } from "react-router-dom"
 import { auth, googleProvider } from "../firebase.js"
 import { signInWithPopup } from "firebase/auth"
@@ -13,14 +13,14 @@ function LogIn() {
   const navigate = useNavigate()
   const [formValue, setFormValue] = useState({
     email: "",
-    password:""
+    password: ""
   })
   const onChange = (e) => {
     setFormValue({ ...formValue, [e.target.name]: e.target.value })
   }
 
 
-  const handleLogin =  async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault()
     dispatch(login(formValue))
 
@@ -35,7 +35,7 @@ function LogIn() {
     navigate("/")
   }
 
-  
+
   const handleSignWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider)
@@ -57,37 +57,50 @@ function LogIn() {
     catch (err) {
       console.log(err)
     }
- }
+  }
 
   return (
- 
-    <div className ="wrapper">
-        <div className ="logo">
-          <h3>Login</h3>
+
+    <div className="wrapper">
+      <div className="logo">
+        <h3>Login</h3>
+      </div>
+
+      <form className="p-3 mt-3" onSubmit={handleLogin}>
+        <div className="form-field d-flex align-items-center">
+          <span className="far fa-user"></span>
+          <input type="email" name="email" id="email" placeholder="Email" onChange={onChange} value={formValue.email} required />
         </div>
-       
-        <form className ="p-3 mt-3" onSubmit={handleLogin}>
-            <div className ="form-field d-flex align-items-center">
-                <span className ="far fa-user"></span>
-                <input type="email" name="email" id="email" placeholder="Email" onChange={onChange} value={formValue.email} required/>
-            </div>
-            <div className ="form-field d-flex align-items-center">
-                <span className ="fas fa-key"></span>
-                <input type="password" name="password" id="pwd" placeholder="Password" onChange={onChange} value={formValue.password} minLength="4" maxLength="20" required/>
-            </div>
-            <button className ="btn mt-3" type="submit">Continue</button>
+        <div className="form-field d-flex align-items-center">
+          <span className="fas fa-key"></span>
+          <input type="password" name="password" id="pwd" placeholder="Password" onChange={onChange} value={formValue.password} minLength="4" maxLength="20" required />
+        </div>
+        <button className="btn mt-3" type="submit">Continue</button>
       </form >
       <div className="divider">
-            <span>or</span>
-          </div>
-      <div className="google-btn" onClick={handleSignWithGoogle}>
-            <img src="/google.png" width="25px" height="26px" style={{ borderRadius: "6px", margin: "0 3px" }} />
-            <button>Continue with Google</button>
-          </div>
-        <div className ="text-center fs-6">
-            <p >Don&apos;t have an account? </p> <a href="/sign-up">Sign up</a>
-        </div>
+        <span>or</span>
       </div>
+      <div
+        onClick={handleSignWithGoogle}
+        className="flex justify-center items-center h-[37px] rounded-md mx-auto mb-10 max-w-[300px] shadow-[3px_3px_3px_#b1b1b1,-3px_-3px_3px_#fff]"
+      >
+        <img
+          src="/google.png"
+          width="25"
+          height="26"
+          className="rounded-md mx-1"
+          alt="Google"
+        />
+        <button
+          className="px-5 py-2 text-black text-[15px] font-medium bg-transparent outline-none border-none"
+        >
+          Continue with Google
+        </button>
+      </div>
+      <div className="text-center text-sm mt-4">
+        <p >Don&apos;t have an account? </p> <a href="/sign-up">Sign up</a>
+      </div>
+    </div>
   )
 }
 
--- user/Frontend/src/pages/SignUp.jsx
+++ user/Frontend/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import { useState } from 'react';
 import { useDispatch } from "react-redux";
 import { register } from '../redux/authSlice';
@@ -13,10 +13,10 @@ function SignUp() {
 
   const [formValue, setFormValue] = useState({
     email: "",
-    password:""
+    password: ""
   })
   const onChange = (e) => {
-    setFormValue({...formValue, [e.target.name]:e.target.value})
+    setFormValue({ ...formValue, [e.target.name]: e.target.value })
   }
 
   const handleSignUp = (e) => {
@@ -25,12 +25,12 @@ function SignUp() {
     setFormValue({
       email: "",
       password: ""
-  });
+    });
     navigate("/login")
   }
 
-  
-  
+
+
   const handleSignWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider)
@@ -52,38 +52,92 @@ function SignUp() {
     catch (err) {
       console.log(err)
     }
- }
+  }
 
-  
+
   return (
-    <div className ="wrapper">
-    <div className ="logo">
-      <h3>Welcome</h3>
-    </div>
-   
-    <form className ="p-3 mt-3" onSubmit={handleSignUp}>
-        <div className ="form-field d-flex align-items-center">
-            <span className ="far fa-user"></span>
-          <input type="email" name="email" id="email" placeholder="Email" value={formValue.email} onChange={onChange} required />
+    <div className="max-w-[400px] min-h-[460px] mx-auto my-10 px-10 py-10 bg-[#ecf0f3] rounded-[15px] shadow-[13px_13px_20px_#cbced1,-13px_-13px_20px_#fff]">
+      {/* Logo */}
+      <div className="text-center mb-4">
+        <h3 className="text-lg font-semibold">Welcome</h3>
+      </div>
+
+      {/* Form */}
+      <form className="mt-3" onSubmit={handleSignUp}>
+        {/* Email */}
+        <div className="flex items-center mb-4 p-0.5 pl-3 rounded-[20px] shadow-[inset_8px_8px_8px_#cbced1,inset_-8px_-8px_8px_#fff]">
+          <span className="far fa-user text-gray-600 mr-2"></span>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={formValue.email}
+            onChange={onChange}
+            required
+            className="w-full bg-transparent outline-none text-gray-600 text-base py-2 px-2"
+          />
         </div>
-        <div className ="form-field d-flex align-items-center">
-            <span className ="fas fa-key"></span>
-            <input type="password" name="password" id="pwd" placeholder="Password" value={formValue.password} onChange={onChange} minLength="4" maxLength="20" required />
+
+        {/* Password */}
+        <div className="flex items-center mb-4 p-0.5 pl-3 rounded-[20px] shadow-[inset_8px_8px_8px_#cbced1,inset_-8px_-8px_8px_#fff]">
+          <span className="fas fa-key text-gray-600 mr-2"></span>
+          <input
+            type="password"
+            name="password"
+            id="pwd"
+            placeholder="Password"
+            value={formValue.password}
+            onChange={onChange}
+            minLength="4"
+            maxLength="20"
+            required
+            className="w-full bg-transparent outline-none text-gray-600 text-base py-2 px-2"
+          />
         </div>
-        <button className ="btn mt-3" type='submit' >SignUp</button>
+
+        {/* Button */}
+        <button
+          type="submit"
+          className="w-full h-10 mt-3 rounded-full bg-[#2e3577] text-white font-medium tracking-[1.3px] shadow-[3px_3px_3px_#b1b1b1,-3px_-3px_3px_#fff] hover:bg-[#1c5f81] transition-colors"
+        >
+          SignUp
+        </button>
       </form>
-      
-  <div className="divider">
-        <span>or</span>
+
+
+
+      <div className="relative flex justify-center my-6 text-gray-400 z-10">
+        <span className="relative inline-block bg-[#ecf0f3] px-3 z-10
+    before:content-[''] before:absolute before:top-1/2 before:left-[-140px] before:w-[130px] before:h-px before:bg-gray-300 before:-z-10
+    after:content-[''] after:absolute after:top-1/2 after:right-[-140px] after:w-[130px] after:h-px after:bg-gray-300 after:-z-10"
+        >
+          or
+        </span>
       </div>
-  <div className="google-btn" onClick={handleSignWithGoogle }>
-        <img src="/google.png" width="25px" height="26px" style={{ borderRadius: "6px", margin: "0 3px" }} />
-        <button>Continue with Google</button>
+
+      <div
+        onClick={handleSignWithGoogle}
+        className="flex justify-center items-center h-[37px] rounded-md mx-auto mb-8 max-w-[300px] shadow-[3px_3px_3px_#b1b1b1,-3px_-3px_3px_#fff]"
+      >
+        <img
+          src="/google.png"
+          width="25"
+          height="26"
+          className="rounded-md mx-1"
+          alt="Google"
+        />
+        <button
+          className="px-5 py-2 text-black text-[15px] font-medium bg-transparent outline-none border-none"
+        >
+          Continue with Google
+        </button>
       </div>
-    <div className ="text-center fs-6">
-        <p >Already have account? </p> <a href="/login">Login</a>
+
+      <div className="text-center text-sm">
+        <p >Already have account? </p> <a href="/login" className='text-[#03A9F4] hover:text-[#039BE5] text-sm'>Login</a>
+      </div>
     </div>
-  </div>
   )
 }
 
