1237f40070007b444e70ac8a3f4a974a260fdde0
Rut Birhanu <birhanurut@gmail.com>
2025-10-04 22:36:39 +0300

    Kafka producer setup

---
+++ user/Backend/config/kafkaConfig.js
@@ -0,0 +1,23 @@
+const Kafka = require("kafkajs")
+
+const kafka = new Kafka(
+    {
+        clientId: "kafka-service",
+        broker:["localhost:9094"]
+    }
+)
+
+const kafkaAdmin = kafka.admin()
+const run = async() => {
+    await kafkaAdmin.connect()
+    await kafkaAdmin.createTopic({
+        topics: [
+            {topic :"payment"},
+            {topic :"order"},
+            {topic :"inventory"},
+            {topic :"notification"},
+        ]
+    })
+}
+
+module.exports = run
