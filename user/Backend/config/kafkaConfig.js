42e238b51f919d8fdab0e89d25807870a29133e4
Rut Birhanu <birhanurut@gmail.com>
2025-10-06 17:01:18 +0300

    refactor Kafka client and producer setup

--- user/Backend/config/kafkaConfig.js
+++ user/Backend/config/kafkaConfig.js
@@ -1,23 +1,90 @@
 const Kafka = require("kafkajs")
 
-const kafka = new Kafka(
-    {
-        clientId: "kafka-service",
-        brokers:["localhost:9094"]
-    }
-)
-
-const kafkaAdmin = kafka.admin()
-const run = async() => {
-    await kafkaAdmin.connect()
-    await kafkaAdmin.createTopic({
-        topics: [
-            {topic :"payment"},
-            {topic :"order"},
-            {topic :"inventory"},
-            {topic :"notification"},
-        ]
-    })
+export const createKafkaClient = (service) => {
+   return new Kafka(
+        {
+            clientId: service,
+            brokers: ["localhost:9094"]
+        }
+    )
 }
 
-module.exports = run
+
+export const createProducer = (kafka) => {
+    const producer = kafka.producer()
+
+    const connect = async () => {
+        await producer.connect()
+    }
+
+    const send = async (topic, message) => {
+        await producer.send({
+            topic,
+            messages: [{ value: JSON.stringify(message) }]
+        })
+    }
+
+    const disconnect = async () => {
+        await producer.disconnect()
+    }
+
+    return { connect, send, disconnect }
+}
+
+
+
+
+export const createConsumer = (kafka, groupId) => {
+    const consumer = kafka.consumer({ groupId })
+
+    const connect = async () => {
+        await consumer.connect()
+    }
+
+    const subscribe = async (topic, handler) => {
+        await consumer.subscribe({
+            topic,
+            fromBeginning: true,
+            messages: [{ value: JSON.stringify(message) }]
+        })
+
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                try {
+                    const value = message.value.toString
+                    if (value) {
+                        handler(JSON.parse(value))
+                    }
+                }
+                catch (err) {
+                    console.log("consumer run error")
+                    console.log(err)
+                }
+            }
+        })
+    }
+
+    const disconnect = async () => {
+        await consumer.disconnect()
+    }
+
+    return { connect, subscribe, disconnect }
+}
+
+
+
+
+
+// const kafkaAdmin = kafka.admin()
+
+// const run = async () => {
+//     await kafkaAdmin.connect()
+//     await kafkaAdmin.createTopic({
+//         topics: [
+//             { topic: "payment" },
+//             { topic: "order" },
+//             { topic: "inventory" },
+//             { topic: "notification" },
+//         ]
+//     })
+// }
